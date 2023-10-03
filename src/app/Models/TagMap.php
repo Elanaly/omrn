72deaac2b6b0e01a6a76589a7d97c79abfadc4a3
Elanaly <drachandayo@gmail.com>
2023-10-03 08:10:41 +0000

    first commit

---
+++ src/app/Models/TagMap.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TagMap extends Model
+{
+    use HasFactory;
+}
